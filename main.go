a0fd2a7fdc9fd3373f22ae8a49a251acaef1b07e
Vedant Kakde <69970950+vedant-kakde@users.noreply.github.com>
2022-10-14 21:10:34 +0530

    Use current namespace

--- main.go
+++ main.go
@@ -21,16 +21,26 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+func getDefaultNs() (string, error) {
+	clientCfg, err := clientcmd.NewDefaultClientConfigLoadingRules().Load()
+	if err != nil {
+		return "", err
+	}
+	namespace := clientCfg.Contexts[clientCfg.CurrentContext].Namespace
+
+	if namespace == "" {
+		namespace = "default"
+	}
+	return namespace, nil
+}
+
 // TODO pick the defautnamespace from the kubeconfig file
 
-// add complex stuff like volumes 
+// add complex stuff like volumes
 
 // test compose files - automate that process of running the main file
 
-
-func main(){
-	fmt.Printf("apiv1.NamespaceAll: %v\n", apiv1.NamespaceAll)
-
+func main() {
 	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
 	if err != nil {
 		panic(err.Error())
@@ -40,8 +50,15 @@ func main(){
 		panic(err)
 	}
 
+	namespace, err := getDefaultNs()
+	if err != nil {
+		error := fmt.Errorf("[error] Can't get current namespace for the context %s", err.Error())
+		fmt.Println(error)
+		return
+	}
+
 	l, err := loader.GetLoader("compose")
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -50,36 +67,36 @@ func main(){
 	}
 
 	opt := kobject.ConvertOptions{
-		CreateD: true,
+		CreateD:                true,
 		CreateDeploymentConfig: true,
-		Volumes: "persistentVolumeClaim",
-		Replicas: 1,
-		Provider: "kubernetes",
-		InputFiles: []string{"docker-compose.yml"},
+		Volumes:                "persistentVolumeClaim",
+		Replicas:               1,
+		Provider:               "kubernetes",
+		InputFiles:             []string{"compose.yml"},
 	}
 
 	komposeObj, err = l.LoadFile(opt.InputFiles)
 
-	if(err != nil){
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	t := &kubernetes.Kubernetes{Opt: opt}
-	
+
 	objects, err := t.Transform(komposeObj, opt)
-	
-	if(err != nil){
+
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
-		
+	deploymentsClient := clientset.AppsV1().Deployments(namespace)
+
 	// dep :=  objects[2].(*appsv1.Deployment)
 	// fmt.Printf("%v\n", dep)
 
 	deploymentObjs := make([]*appsv1.Deployment, 0)
 	for i := 0; i < len(objects); i++ {
-		if(objects[i].GetObjectKind().GroupVersionKind().Kind == "Deployment"){
+		if objects[i].GetObjectKind().GroupVersionKind().Kind == "Deployment" {
 			depl := objects[i].(*appsv1.Deployment)
 			deploymentObjs = append(deploymentObjs, depl)
 		}
@@ -100,8 +117,8 @@ func main(){
 		svc := *t.CreateService(key.Interface().(string), komposeObj.ServiceConfigs[key.Interface().(string)])
 		serviceObjs = append(serviceObjs, &svc)
 	}
-	
-	servicesClient := clientset.CoreV1().Services(apiv1.NamespaceDefault)
+
+	servicesClient := clientset.CoreV1().Services(namespace)
 
 	for i := 0; i < len(serviceObjs); i++ {
 		result, err := servicesClient.Create(context.TODO(), serviceObjs[i], metav1.CreateOptions{})
